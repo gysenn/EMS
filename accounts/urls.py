c4bc3083c7353b9293812f154d01e6f9e4760243
gysenn <ghanashyamshrestha340283@gmail.com>
2025-12-27 18:34:36 +0545

    Updated project by adding feature to delete employee by managera and to delete both employee and manager by admin and add password change feature for each user

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('change-password/', views.change_password, name='change_password'),
 ]
